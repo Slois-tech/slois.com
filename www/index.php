9301e733038c66e6f88e58f68385035563c64725
Ybelyavsky <belyavsky@outlook.com>
2017-01-18 13:40:03 +0300

    Старт

---
+++ www/index.php
@@ -0,0 +1,107 @@
+<!DOCTYPE html>
+<html lang="ru">
+<head>
+	<!-- Кодировка сайта --><meta charset="utf8" />
+	<!-- Latest compiled and minified CSS --><link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.7/css/bootstrap.min.css" integrity="sha384-BVYiiSIFeK1dGmJRAkycuHAHRg32OmUcww7on3RYdg4Va+PmSTsz/K68vbdEjh4u" crossorigin="anonymous">
+	<!-- Optional theme --><link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.7/css/bootstrap-theme.min.css" integrity="sha384-rHyoN1iRsVXV4nD0JutlnGaslCJuC7uwjduW9SVrLvRYooPp2bWYgmgJQIXwl/Sp" crossorigin="anonymous">
+	
+	<!-- Заголовок --><title>Slois framework</title>	
+	<!-- Шрифт --><link href="https://fonts.googleapis.com/css?family=Roboto+Condensed:300,300i,400,400i,700,700i" rel="stylesheet">
+	<!-- Мои стили --><link rel='stylesheet' href='/main.css'>
+	<!-- Дескрипшен --><meta name="description" content="Образование в Польше">
+	<!-- Кейвордс --><meta name="keywords" content="Образование в Польше">
+</head>
+
+<body>
+
+<!-- Резиновая обертка --><div class="container-fluid"> 
+
+<!-- Отсуп между разделами --><br>
+
+<!-- Шапка --> 
+<div class="row">
+	<div class="col-md-12">
+		<h1 align="center">SLOIS framework</h1>
+	</div> 
+</div> 
+<!-- .Шапка --> 
+
+
+
+
+
+
+	
+<!-- Содержание --> 
+<div class="row">
+	<div class="col-md-12">
+		<button type="button" class="btn btn-success btn-lg">
+			<a href="https://github.com/Slois-tech/SLOIS-framework">Скачать</a> 
+		</button>
+	</div> 
+</div> 
+<!-- .Содержание --> 
+
+	
+
+<!-- Комментарии --> 
+<div id="disqus_thread"></div>
+<script>
+
+/**
+*  RECOMMENDED CONFIGURATION VARIABLES: EDIT AND UNCOMMENT THE SECTION BELOW TO INSERT DYNAMIC VALUES FROM YOUR PLATFORM OR CMS.
+*  LEARN WHY DEFINING THESE VARIABLES IS IMPORTANT: https://disqus.com/admin/universalcode/#configuration-variables*/
+/*
+var disqus_config = function () {
+this.page.url = PAGE_URL;  // Replace PAGE_URL with your page's canonical URL variable
+this.page.identifier = PAGE_IDENTIFIER; // Replace PAGE_IDENTIFIER with your page's unique identifier variable
+};
+*/
+(function() { // DON'T EDIT BELOW THIS LINE
+var d = document, s = d.createElement('script');
+s.src = '//slois-com.disqus.com/embed.js';
+s.setAttribute('data-timestamp', +new Date());
+(d.head || d.body).appendChild(s);
+})();
+</script>
+<noscript>Please enable JavaScript to view the <a href="https://disqus.com/?ref_noscript">comments powered by Disqus.</a></noscript>
+<!-- .Комментарии --> 
+
+
+
+<!-- Подвал --> 
+<div class="row">
+	<div class="col-md-12">
+		© 2017 SLOIS framework
+	</div> 
+</div> 
+
+<!-- .Подвал -->	
+		
+		
+<!-- .Конец резиновой обертки --></div>
+
+
+<!-- Счетчики --> 
+<!-- Yandex.Metrika counter --> <script type="text/javascript"> (function (d, w, c) { (w[c] = w[c] || []).push(function() { try { w.yaCounter42189049 = new Ya.Metrika({ id:42189049, clickmap:true, trackLinks:true, accurateTrackBounce:true, trackHash:true }); } catch(e) { } }); var n = d.getElementsByTagName("script")[0], s = d.createElement("script"), f = function () { n.parentNode.insertBefore(s, n); }; s.type = "text/javascript"; s.async = true; s.src = "https://mc.yandex.ru/metrika/watch.js"; if (w.opera == "[object Opera]") { d.addEventListener("DOMContentLoaded", f, false); } else { f(); } })(document, window, "yandex_metrika_callbacks"); </script> <noscript><div><img src="https://mc.yandex.ru/watch/42189049" style="position:absolute; left:-9999px;" alt="" /></div></noscript> <!-- /Yandex.Metrika counter -->
+<!-- Гугл --> 
+<script>
+  (function(i,s,o,g,r,a,m){i['GoogleAnalyticsObject']=r;i[r]=i[r]||function(){
+  (i[r].q=i[r].q||[]).push(arguments)},i[r].l=1*new Date();a=s.createElement(o),
+  m=s.getElementsByTagName(o)[0];a.async=1;a.src=g;m.parentNode.insertBefore(a,m)
+  })(window,document,'script','https://www.google-analytics.com/analytics.js','ga');
+
+  ga('create', 'UA-90560619-1', 'auto');
+  ga('send', 'pageview');
+
+</script>
+<!-- .Гугл --> 
+<!-- .Счетчики --> 
+
+
+<!-- Disqus --> <script id="dsq-count-scr" src="//slois-com.disqus.com/count.js" async></script>
+<!-- Jquery --><script src="https://ajax.googleapis.com/ajax/libs/jquery/3.1.1/jquery.min.js"></script>
+<!-- Latest compiled and minified JavaScript --><script src="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.7/js/bootstrap.min.js" integrity="sha384-Tc5IQib027qvyjSMfHjOMaLkfuWVxZxUPnCJA7l2mCWNIpG9mGCD8wGNIcPD7Txa" crossorigin="anonymous"></script>
+
+</body>
+</html>
